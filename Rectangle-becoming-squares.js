33146ba221ba9707ff1c16e666d1f89a5b3fb611
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-16 19:58:11 -0800

    Create Rectangle-becoming-squares.js

---
+++ Rectangle-becoming-squares.js
@@ -0,0 +1,11 @@
+/*
+You will be given two dimensions:
+  a positive integer length
+  a positive integer width
+
+Output: You will return a string. 
+
+Examples:
+  sqInRect(5, 3) should return [3, 2, 1, 1]
+  sqInRect(3, 5) should return [3, 2, 1, 1]
+*/
